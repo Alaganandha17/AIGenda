5f4ca22b2210b3ea5a44082334ccaef7e28ddf64
Alaganandha17 <17nandha17@gmail.com>
2026-04-19 14:49:57 +0530

    Remove unnecessary import of React

--- ai-genda/src/components/EventContext.js
+++ ai-genda/src/components/EventContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import  { createContext, useContext } from "react";
 
 export const EventContext = createContext();
 export const useEvent = () => useContext(EventContext);
